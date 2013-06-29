09e98bbff7b7eee1b43e3a68ec58d72d454dcf8f
George Bonner <georgebonnr@gmail.com>
2013-06-29 02:07:40 -0500

    updated my_app.rb

--- my_app.rb
+++ my_app.rb
@@ -1,6 +1,6 @@
 # my_app.rb
 class MyApp
   def call env
-    [200, {"Content-Type" => "text/html"}, ["Hello Rack Participants"]] 
+    [200, {"Content-Type" => "text/html"}, ["Welcome to George's sample rack app!"]] 
   end
 end
